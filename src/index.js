dab6585a854824b2e110822d8e982e3271377fbc
EC2 Default User <ec2-user@ip-172-31-40-178.eu-west-1.compute.internal>
2020-06-18 21:26:15 +0000

    Added font support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Typography from '@material-ui/core/Typography';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Typography>
+            <App />
+        </Typography>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
