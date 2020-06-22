45225acce526216fef142f97da1dffd7672ed41e
LeonieEbert <ebert.leonie@gmail.com>
2020-06-22 22:37:22 +0200

    Home-Button bei Quizfragen + Anzeige des Quizfortschritts

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import { connect } from "react-redux";
 import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
 
 import Answer from './answer'
 import Explanation from './explanation'
+import { changeToView, VIEWS } from '../state';
 
 
 const mapStateToProps = (state) => {
     return { 
         currentTask: state.currentTask,
+        currentTaskIndex: state.currentTaskIndex,
+        numberOfTasks: state.numberOfTasks,
     };
 };
+
+function mapDispatchToProps(dispatch) {
+    return {
+        changeToView: (view) => dispatch(changeToView(view))
+    };
+}
 
 class Quiz extends React.Component {
 
     render() {
         return (
+           
             <Box>
+            <div>
+                <div>
+                    <IconButton  aria-label="home" onClick={() => {this.props.changeToView(VIEWS.MENU)}}>
+                        <HomeIcon />
+                    </IconButton> 
+                 </div>
+                 <div style={{ textAlign: 'center' }}>
+                    { this.props.currentTaskIndex }/{ this.props.numberOfTasks }
+                 </div>
+            </div>
+   
                 <p>{ this.props.currentTask.question }</p>
                 
                 {Object.keys(this.props.currentTask.answers).map(key => 
@@ -29,4 +52,4 @@ class Quiz extends React.Component {
     }
 }
 
-export default connect(mapStateToProps)(Quiz);
+export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
